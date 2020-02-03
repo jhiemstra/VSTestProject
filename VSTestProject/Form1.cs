01f9de2251abae763d3b49e84136f70b828011fb
Josh Hiemstra <jhiemstra37@gmail.com>
2020-02-03 09:51:50 -0600

    Updated project to test VS push

--- VSTestProject/Form1.cs
+++ VSTestProject/Form1.cs
@@ -16,5 +16,11 @@ namespace VSTestProject
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var test = "Hello";
+            textBox1.Text = test;
+        }
     }
 }
--- VSTestProject/Program.cs
+++ VSTestProject/Program.cs
@@ -17,6 +17,8 @@ namespace VSTestProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //Comment Test
         }
     }
 }
